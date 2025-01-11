7abd259c7d0b1e8892707a91044e358bd610ad03
AG2704 <129029347+AG2704@users.noreply.github.com>
2025-01-11 14:22:58 +0530

    Update getAIworking.cs

--- Assets/getAIworking.cs
+++ Assets/getAIworking.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class getAIworking : MonoBehaviour
 {
-    private string apikey = "";
+    private string apikey = "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";
     // Start is called before the first frame update
     void Start()
     {
